5ea04c2bcdc8f35127507367172f4ce71c4347dd
Julia Lepner <ylepner@gmail.com>
2023-07-03 12:38:56 +0200

    feat: start server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('HEllo!')
+import 'dotenv/config';
+import { createServer as createServerHttp } from 'http';
+
+const PORT = process.env.PORT;
+
+const myServer = createServerHttp((req, res) => {
+  res.end('Request accepted');
+});
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
