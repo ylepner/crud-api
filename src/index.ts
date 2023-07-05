2efbffbae2408df61f8313e9d0fc8a419c4ded5c
Julia Lepner <ylepner@gmail.com>
2023-07-05 16:12:17 +0200

    feat: add get method

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,12 @@
 import 'dotenv/config';
-import { ServerResponse, createServer } from 'http';
+import { IncomingMessage, ServerResponse, createServer } from 'http';
 import { UserService } from './user.service';
 
-const PORT = process.env.PORT;
-
-
-// +10 GET api/users
-// +10 GET api/users/{userId}
-// +10 POST api/users
-// +10 PUT api/users/{userId}
-// +10 DELETE api/users/{userId}
-
-
-interface RouteEntry {
-  method: 'GET' | 'POST' | 'PUT';
-  url: RegExp;
-  execFn: (param: any, body: any) => any
-}
-
-const routes: RouteEntry[] = [
-  { method: 'GET', url: /^\/api\/users$/, execFn: () => service.getUsers() },
-  { method: 'GET', url: /^\/api\/users\/(.+)$/, execFn: (userId) => service.getUser(userId) },
-  { method: 'POST', url: /^\/api\/users$/, execFn: (_, body) => service.addUser(body) }
-]
-
+const PORT = process.env.PORT || 4000;
+const uuid = '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed';
 
 const service = new UserService();
-const myServer = createServer((req, res) => {
+const myServer = createServer(async (req, res) => {
   const route = routes.find(x => x.method === req.method && req.url?.match(x.url));
   if (!route) {
     res.writeHead(404);
@@ -36,12 +16,50 @@ const myServer = createServer((req, res) => {
   }
   const param = req.url!.match(route.url)?.[0];
   try {
-    const result = route.execFn(param, {});
-    writeData(result, 200, res);
+    const result = await route.execFn(param, req);
+    writeData(result.data, result.code, res);
 
   } catch (e) {
     writeData(null, 404, res);
   }
+})
+
+interface RouteEntry {
+  method: 'GET' | 'POST' | 'PUT';
+  url: RegExp;
+  execFn: (param: any, request: IncomingMessage) => Promise<HttpResponse>
+}
+
+const routes: RouteEntry[] = [
+  {
+    method: 'GET', url: /^\/api\/users\/?$/, execFn: async () => {
+
+      return {
+        data: service.getUsers(),
+        code: 200
+      }
+    }
+  },
+  {
+    method: 'GET', url: /^\/api\/users\/(.+)$/, execFn: async (userId) => {
+      const result = service.getUser(userId);
+      if (!result) {
+        return {
+          data: 'not found',
+          code: 404
+        }
+      }
+      return {
+        data: result,
+        code: 200
+      }
+    }
+  },
+  // { method: 'POST', url: /^\/api\/users$/, execFn: (_, body) => service.addUser(body) }
+]
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 function writeData(data: any | undefined, code: number, res: ServerResponse) {
@@ -53,6 +71,7 @@ function writeData(data: any | undefined, code: number, res: ServerResponse) {
   res.end();
 }
 
-myServer.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+interface HttpResponse {
+  code: number;
+  data: any;
+}
