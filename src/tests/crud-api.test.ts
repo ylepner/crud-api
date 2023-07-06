21422c63f4ff392a99787eb4d122e2d71e6deae5
Julia Lepner <ylepner@gmail.com>
2023-07-06 23:56:30 +0200

    feat: add bad request tests

--- src/tests/crud-api.test.ts
+++ src/tests/crud-api.test.ts
@@ -6,11 +6,12 @@ import { Server } from 'http';
 const PORT = process.env.PORT || 4000;
 
 const apiRequest = request(`http://localhost:${PORT}/api`);
-console.log('Im doing something here')
+
+let server: Server;
+let userId: String;
 
 describe('Tests with api methods', () => {
-  let server: Server;
-  let userId: String;
+
   beforeAll(() => {
     server = createMyServer(new UserService()).listen(PORT, () => {
       console.log(`Server is listening on port ${PORT}`);
@@ -59,3 +60,44 @@ describe('Tests with api methods', () => {
   }
   )
 });
+
+describe('Test bad request', () => {
+  let server: Server;
+  let userId: String;
+  const testUser = {
+    username: "Test Name",
+    age: 56,
+    hobbies: ["reading", "swimming"]
+  }
+  beforeAll(() => {
+    server = createMyServer(new UserService()).listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  });
+  afterAll(() => {
+    server.close();
+  });
+
+  it('Should answer with status code 404 if request sends to non-existing endpoints', async () => {
+    (await apiRequest.get('/user').expect(404))
+  });
+
+  it('Should answer with status code 400 if POST request body does not contain required fields', async () => {
+    const testUser =
+    {
+      username: "Test Name",
+      hobbies: ["reading", "swimming"]
+    }
+    apiRequest.post('/users').send(testUser).expect(400);
+  })
+
+  it('Should answer with status code 400 if userId is invalid (not uuid) with PUT request', async () => {
+    const notUuid = '21r23r23t4t';
+    await apiRequest.put(`/users/${notUuid}`).send(testUser).expect(400);
+  })
+
+  it(`Should answer with status code 404 if userId doesn't exist`, async () => {
+    const fakeUuid = '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d';
+    await apiRequest.put(`/users/${fakeUuid}`).send(testUser).expect(404);
+  })
+})
